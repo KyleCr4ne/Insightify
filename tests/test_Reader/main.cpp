7e16df19678d77f2680f38f7324f36e523963467
Ramil Latypov <albedo@MacBook-Air-Ramil.local>
2024-04-15 00:25:07 +0500

    Example of Reader

---
+++ tests/test_Reader/main.cpp
@@ -0,0 +1,30 @@
+#include "./../../include/Reader/Reader.hpp"
+#include "./../../src/Reader/Reader.cpp"
+
+#include <vector>
+#include <iostream>
+
+
+int main() {
+    Insightify::Reader::ReadFile<double> Reader;
+
+    std::vector <std::vector<double>> X;
+    std::vector<double> Y;
+
+    Reader.readCSV("in.csv", false);
+    
+    X = Reader.data;
+    Y = Reader.targetValues;
+
+    std::cout << "Features of objects:" << std::endl;
+    for (const auto &row : X) {
+        for (const auto &elem : row) {
+            std::cout << elem << "\t";
+        }
+        std::cout << std::endl;
+    }
+    std::cout << "Target value of objects" << std::endl;
+    for (const auto &target : Y) {
+        std::cout << target << std::endl;
+    }
+}
